65b2711232f7f6d0b22cbaf2fbdb32738bcd775c
k16ds01 <danielle.sarafian16@kzoo.edu>
2018-04-03 23:20:49 -0400

    code::blocks commit

---
+++ main.c
@@ -0,0 +1,17 @@
+/**
+ * Hello World
+ *
+ * Author:  Danielle Sarafian
+ *
+ * Creation Date:  3 April 2018
+ *
+ * Prints "Hello world"
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
